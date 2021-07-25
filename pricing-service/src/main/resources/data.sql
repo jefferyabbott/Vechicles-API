9da48d61407760c00280a982b32213a413873138
Jeffery Abbott <jeff@jefferyabbott.com>
2021-07-24 18:45:49 -0700

    Create pricing-service microservice

---
+++ pricing-service/src/main/resources/data.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS PRICE (vehicleId INT PRIMARY KEY auto_increment, currency VARCHAR(3), price DECIMAL(10,2));
+
+INSERT INTO price (vehicleId, currency, price) VALUES (1, 'USD', 21952.73);
+INSERT INTO price (vehicleId, currency, price) VALUES (2, 'USD', 32918.15);
+INSERT INTO price (vehicleId, currency, price) VALUES (3, 'USD', 19342.85);
+INSERT INTO price (vehicleId, currency, price) VALUES (4, 'USD', 20999.99);
+INSERT INTO price (vehicleId, currency, price) VALUES (5, 'USD', 27655.99);
+INSERT INTO price (vehicleId, currency, price) VALUES (6, 'USD', 22944.55);
+INSERT INTO price (vehicleId, currency, price) VALUES (7, 'USD', 22405.12);
+INSERT INTO price (vehicleId, currency, price) VALUES (8, 'USD', 42512.14);
+INSERT INTO price (vehicleId, currency, price) VALUES (9, 'USD', 31444.45);
+INSERT INTO price (vehicleId, currency, price) VALUES (10, 'USD', 26995.00);
+INSERT INTO price (vehicleId, currency, price) VALUES (11, 'USD', 18944.50);
+INSERT INTO price (vehicleId, currency, price) VALUES (12, 'USD', 19799.00);
+INSERT INTO price (vehicleId, currency, price) VALUES (13, 'USD', 23890.10);
+INSERT INTO price (vehicleId, currency, price) VALUES (14, 'USD', 22467.90);
+INSERT INTO price (vehicleId, currency, price) VALUES (15, 'USD', 21550.00);
+INSERT INTO price (vehicleId, currency, price) VALUES (16, 'USD', 19999.99);
+INSERT INTO price (vehicleId, currency, price) VALUES (17, 'USD', 24500.75);
+INSERT INTO price (vehicleId, currency, price) VALUES (18, 'USD', 30000.00);
+INSERT INTO price (vehicleId, currency, price) VALUES (19, 'USD', 32750.00);
+INSERT INTO price (vehicleId, currency, price) VALUES (20, 'USD', 29950.85);
+
