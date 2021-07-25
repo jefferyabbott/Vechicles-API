e1a26191580c501c8e62321833cdd47b694d9129
Jeffery Abbott <jeff@jefferyabbott.com>
2021-07-25 11:34:23 -0700

    Add param to repository interface

--- pricing-service/src/main/java/com/udacity/pricing/domain/price/PriceRepository.java
+++ pricing-service/src/main/java/com/udacity/pricing/domain/price/PriceRepository.java
@@ -1,9 +1,13 @@
 package com.udacity.pricing.domain.price;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PriceRepository extends CrudRepository<Price, Long> {
 
+    List<Price> findByVehicleId(@Param("vehicleId") Long vehicleId);
 }
